75bfbfd872971830d52b0fdae256c8d203adc1ec
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-04-26 21:52:21 -0700

    capturing DOM source code

--- popup.js
+++ popup.js
@@ -95,7 +95,7 @@ function renderStatus(statusText) {
 document.addEventListener('DOMContentLoaded', function() {
   getCurrentTabUrl(function(url) {
     // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
+    renderStatus();
 
     getImageUrl(url, function(imageUrl, width, height) {
 
@@ -116,3 +116,28 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+//******NEW CODE********//
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+  if (request.action == "getSource") {
+    message.innerText = request.source;
+  }
+});
+
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.runtime.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.runtime.lastError.message;
+    }
+  });
+
+}
+
+window.onload = onWindowLoad;
