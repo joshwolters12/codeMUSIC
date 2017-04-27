798230db3a1a2b87cf08ef05f45b122aba9cb8c5
Joshua Wolters <joshuawolters@Joshuas-MacBook-Pro.local>
2017-04-27 02:44:04 -0700

    finished color layout

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 
 function onWindowLoad() {
   var stringText;
+  var totalCount = 0;
   var message = document.querySelector('#message');
 
   chrome.runtime.onMessage.addListener(function(request, sender) {
@@ -11,24 +12,43 @@ function onWindowLoad() {
         }
         
         // stringArray = stringText.split(' ')
-        wordsArray = ["</p>", "/body", "</h1>", "</li>", "</ul>", "</script>"]
+        wordsArray = ["</p>", "</body>", "</h1>", "</li>", "</ul>", "</script>","</div>","</form>"]
         counterObj = {};
 
+        
         wordsArray.forEach((e)=>{
             let count = stringText.split(e).length;
             counterObj[e] = count;
-            console.log(counterObj[e])
+            totalCount += count;
         })
-        
-        message.innerText = counterObj["</p>"]
-        console.log(counterObj["</p>"])
-  
 
+        //add div elements to the popup page
+        for (key in counterObj){
+          let word = $('<div>');
+          word.attr('class','words')
+          word.attr('id',key)
+          heightVal = (counterObj[key] < 5) ? 25 :
+                        (counterObj[key] < 10) ? 50 :
+                          (counterObj[key] < 25) ? 75 :
+                            (counterObj[key] < 100) ? 100 : 125;
+          widthVal = heightVal *2.8;
+          fontSize = 0.4 * heightVal;
+          lineHeight = heightVal;
+          word.css({height: heightVal, width: widthVal, "font-size": fontSize, "line-height": lineHeight+'px'})
+
+          backgroundColor = (counterObj[key] < 5) ?  '#FFD1BF' :
+                              (counterObj[key] < 10) ? '#F8B195':
+                                (counterObj[key] < 25) ? '#F67280' :
+                                  (counterObj[key] < 100) ? '#C06C84' : '#6C5B7B';
+
+
+          word.css({"background-color": backgroundColor})
+          word.text(key) // = key + counterObj[key];
+          $('body').prepend(word)
+        }
 
     });
 
-
-  
   chrome.tabs.executeScript(null, {
     file: "getPagesSource.js"
   }, function() {
@@ -38,7 +58,7 @@ function onWindowLoad() {
     }
   });
 
-    // alert(stringText)
+
       
 }
 
